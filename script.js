c3af14a9c9d52cb07beebd3d7a2779bdd71ae85d
towhid7656 <towhid@npcoding.com>
2025-12-12 19:04:48 +0600

    Made scroll smoother

--- script.js
+++ script.js
@@ -102,5 +102,6 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 let smoother = ScrollSmoother.create({
   smooth: 2,
   effects: true,
-  normalizeScroll: true
+  normalizeScroll: true,
+  smoothTouch: 0.1
 });
